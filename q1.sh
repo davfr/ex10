f4be3c8fbe38af7893d968d8229465bfef9e318d
Shelly <shelly.orenstein@runi.ac.il>
2026-01-22 17:38:19 +0200

    Add q1 and q2 solutions

--- q1.sh
+++ q1.sh
@@ -1,1 +1,16 @@
-This is q1 answer
+#!bin/bash
+#
+#
+awk 'END {print NR}' books/aliceinwonderland.txt 
+
+
+awk '/ Alice / {count++} END {print count}' books/aliceinwonderland.txt 
+
+
+awk '{ for (i=1; i<=NF; i++) print $i }' books/aliceinwonderland.txt | awk '{a[$1]++} END {for (k in a) print a[k], k}' | awk '$1==1 {print}'
+
+
+awk '{ for (i=1; i<=NF; i++) print $i }' books/aliceinwonderland.txt | awk '{a[$1]++} END {for (k in a) print a[k], k}' | sort -nr | head -5
+
+
+awk '{ for (i=1;i<=NF;i++) {sum += length($i); count++ }} END {print sum / count}' books/aliceinwonderland.txt
