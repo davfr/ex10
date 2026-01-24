410f624abea6a91149286e85200682488e292281
Achiad Avigdori <achiad99@gmail.com>
2026-01-24 19:48:53 +0000

    Q1 updated with answers to all five sections

--- q1.sh
+++ q1.sh
@@ -1,1 +1,13 @@
-This is q1 answer
+#!/bin/bash
+
+awk 'END {print NR}' aliceinwonderland.txt
+
+awk '{for (i=1; i<=NF; i++) if ($i=="Alice") c++} END {print c}' aliceinwonderland.txt #I assumed that the qustion asked for each Alice even if it is in the end/beginning of the row else I would have used (i=2; i<=NF-1; i++)
+
+sed -E 's/\s+/\n/g' aliceinwonderland.txt | awk 'NF {a[$1]++} END {for (k in a) print a[k], k}' | awk '$1==1 {print $2}'
+
+sed -E 's/\s+/\n/g' aliceinwonderland.txt | awk 'NF {a[$1]++} END {for (k in a) print a[k], k}' | sort -rn | head -n 5 | awk '{print $2}'
+
+awk '{for(i=1;i<=NF;i++) sum+=length($i); word+=NF} END{print sum/word}' aliceinwonderland.txt #used GPT for the loop in awk
+
+
