ca48cb462db396e6ffcdccc50821dc68c5bce756
Noy <noy.nizri@gmail.com>
2026-01-25 10:48:21 +0000

    Add solution for question 1

--- q1.sh
+++ q1.sh
@@ -1,1 +1,14 @@
-This is q1 answer
+#!/bin/bash
+
+awk END'{print NR}' aliceinwonderland.txt
+
+grep -o " Alice " aliceinwonderland.txt | awk END'{print NR}'
+
+grep -oE ' [[:alpha:]]+ ' aliceinwonderland.txt | sort | uniq -c | awk '$1==1' | wc -l
+
+grep -oE ' [[:alpha:]]+ ' aliceinwonderland.txt | sort | uniq -c | sort -nr | awk ' NR==1 , NR==5 {print NR, $2}'
+
+grep -oE ' [[:alpha:]]+ ' aliceinwonderland.txt |  grep -oE ' [[:alpha:]]+ ' aliceinwonderland.txt | awk '{sum+=length($0) ; count++} END{print sum/count}' 
+
+
+
