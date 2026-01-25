92fe094c83b09c4cf2e4ed120a682df51e0df346
amir <amir.nagar@post.runi.ac.il>
2026-01-25 10:51:33 +0000

    Added q1 Answer

--- q1.sh
+++ q1.sh
@@ -1,1 +1,15 @@
-This is q1 answer
+
+#a
+awk '{print NR}' aliceinwonderland.txt | wc -l
+
+#b 
+grep -o " Alice " aliceinwonderland.txt | wc -l
+
+#c
+grep -Eo " [a-z|A-Z]+ " aliceinwonderland.txt | sort | uniq -c | sort -nr | awk '$1 == 1'
+
+#d
+grep -Eo " [a-z|A-Z]+ " aliceinwonderland.txt | sort | uniq -c | sort -nr | head -n 5
+
+#e
+grep -Eo [[:alpha:]]+ aliceinwonderland.txt | awk '{sum+=length($0) } END{print sum/NR}'
