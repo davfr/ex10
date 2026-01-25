dc23213481582998496635d6410dd9634a52eedd
amit <amitdoyev5@gmail.com>
2026-01-25 15:01:50 +0000

    Add solutions for exercise 10

--- q1.sh
+++ q1.sh
@@ -1,1 +1,5 @@
-This is q1 answer
+awk 'END {print NR}' aliceinwonderland.txt
+awk '/ Alice / {count++} END {print count}' aliceinwonderland.txt
+awk '{a[$1]++} END {for (k in a) {if (a[k]==1) print a[k], k}}' aliceinwonderland.txt 
+awk '{a[$1]++} END {for (k in a) {if (k ~ /[A-Za-z]/) print a[k], k}}' aliceinwonderland.txt | sort -n | tail -5
+awk '{for(i=1;i<=NF;i++) {sum+=length($i);count++}} END {print sum/count}' aliceinwonderland.txt 
