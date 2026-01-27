3737524e8ea26d42f499cf14848f5dd9bded0aaf
Noya Weiss <noya.weiss01@post.runi.ac.il>
2026-01-27 09:57:23 +0000

    Q1: awk command on aliceinwonderland.txt file

--- q1.sh
+++ q1.sh
@@ -1,1 +1,18 @@
-This is q1 answer
+#!/bin/bash
+
+nr=$(awk 'END{print NR}' aliceinwonderland.txt)
+echo "Number of rows: ${nr}"
+
+alice_count=$(awk 'BEGIN{c=0} / Alice /{c+=1} END{print c}' aliceinwonderland.txt)
+echo "Alice count: ${alice_count}"
+
+word_count=$(awk '{total+=NF} END{print total}' aliceinwonderland.txt)
+echo "Total words count: ${word_count}"
+
+top_5=$(cat aliceinwonderland.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | awk '{for(i=1; i<=NF; i++) if ($i ~ /^[[:alpha:]]+$/) dict[$i]++} END{ for(word in dict)  print dict[word], word}' | sort -nr -t' ' -k1 | head -n5 )
+
+echo "Top 5 most repeated words:"
+echo "${top_5}"
+
+avg=$(cat aliceinwonderland.txt | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | awk '{for(i=1; i<=NF; i++) if ($i ~ /^[[:alpha:]]+$/) {total_length+=length($i); total_words++}} END{print total_length/total_words}')
+echo "Average word length: ${avg}"
