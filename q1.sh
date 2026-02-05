9e957f1bb4ad5d481e9356b4e4b684867dfdd081
Snirk9 <snirki@gmail.com>
2026-02-05 10:44:24 +0000

    replaced q1.sh from the main repo to mine, and answered it

--- q1.sh
+++ q1.sh
@@ -1,1 +1,14 @@
-This is q1 answer
+#a
+awk '{print NR}' aliceinwonderland.txt | tail -1
+
+#b
+awk '{ for (i=1; i<=NF; i++) if ($i == "Alice") count++ } END { print count }' aliceinwonderland.txt
+
+#c
+cat aliceinwonderland.txt | tr " " "\n" | awk '{a[$1]++} END {for (k in a) if (a[k]==1) print a[k], k}'
+
+#d
+cat aliceinwonderland.txt | tr " " "\n" | awk '{a[$1]++} END {for (k in a) print a[k], k}' | sort -h | grep -E "[a-zA-Z]" | tail -5
+
+#e
+echo $(awk '{count=count+length($0)} END {print count} ' aliceinwonderland.txt) / $(awk '{ for (i=1; i<=NF; i++) count++ } END { print count }' aliceinwonderland.txt) | bc -l
